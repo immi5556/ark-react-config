140bdda9241756675198f8e14a7c2ca9c5addb29
jiraj <immanuelraj.j@clariant.com>
2021-04-28 18:32:27 +0530

    Updates

---
+++ precache-manifest.356b3f4fa34b1f5b6b3687c1c11b7198.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "493f470e0aae97a4e60f279700b0ac25",
+    "url": "./index.html"
+  },
+  {
+    "revision": "1102a78bb97b68b85dd2",
+    "url": "./static/css/main.42cc8b22.chunk.css"
+  },
+  {
+    "revision": "37c4bf290e73d46d33c6",
+    "url": "./static/js/2.76700f4e.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.76700f4e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1102a78bb97b68b85dd2",
+    "url": "./static/js/main.482d7e9c.chunk.js"
+  },
+  {
+    "revision": "5f6fb71b446703270972",
+    "url": "./static/js/runtime-main.b815fe65.js"
+  }
+]);
